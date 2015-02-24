0210f3dae08ed11df02d7359590dd20fa483209c
gman <naver@naverui-MacBook-Air.local>
2015-02-24 22:14:47 +0900

    [fix] CORS

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
-var dbPool = require('./module/mysql.js');
-var conn = new dbPool(mysql);
+//var mysql = require('mysql');
+//var dbPool = require('./module/mysql.js');
+//var conn = new dbPool(mysql);
 
-app.enable('trust proxy');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/view', express.static(__dirname + '/www/view'));
 app.use('/css', express.static(__dirname + '/www/css'));
 app.use('/img', express.static(__dirname + '/www/img'));
 
 app.get('/', function(req, res){
-    conn.sendConnectQuery(req.ip, req.get('user-agent'));
+    //conn.sendConnectQuery(req.ip, req.get('user-agent'));
     var options = {
         root: __dirname + '/www/view/',
         headers: {
@@ -18,7 +23,7 @@ app.get('/', function(req, res){
             'x-sent': true
         }
     };
-    res.sendfile('index.html', options);
+    res.sendFile('index.html', options);
 });
 
 app.listen(7777);
